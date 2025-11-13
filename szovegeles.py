09de9d359f2933f25ebb76a9c57eef7cbde5a591
MaximMolnar1234 <maxim20060925@gmail.com>
2025-11-13 10:05:17 +0100

    feladat

--- szovegeles.py
+++ szovegeles.py
@@ -6,10 +6,26 @@ except IOError as hiba:
             print(hiba)
     
 # 1.feladat
-lista = []
-szoveg.split("**")
+lista = szoveg.split("**")
 print(szoveg.split("**"))
 
+for i in range(len(lista)):
+    lista[i] = lista[i].strip()
+"""
+for sor in lista:
+    if (len(sor) == 0):
+        lista.remove(sor)
+"""
+db = 0
+for i in range(len(lista)):
+    if (len(lista[i]) > 0):
+        lista[db] = lista[i] 
+        db += 1
+for i in range(db, len(lista)):
+    lista.pop()    
 
+
+print(lista)    
+    
 
     
