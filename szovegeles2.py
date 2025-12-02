646084259eb2ee9f1833296001a0a0036ef221a0
MaximMolnar1234 <maxim20060925@gmail.com>
2025-12-02 10:51:30 +0100

    szovegeles2_2

--- szovegeles2.py
+++ szovegeles2.py
@@ -1,26 +1,29 @@
 import string
-karakterlanc = ""
 
-# fájlbeolvasás
+#1. feladat
+betu = ""
+
 try:
     with open("string_input.txt",encoding = 'utf-8')as fajl:
-        karakterlanc = fajl.read()
-        #print(szoveg)
+        betu = fajl.read()
 except IOError as ex:
     print(ex)
 
-# 1.feladat
-karakterlanc = karakterlanc.strip()    
-# ================================================================    
-    
-# 2. feladat    
-db_szam = 0
-string.digits
+betu = betu.strip()
 
-for karakter in karakterlanc:
-    if (karakter in string.digits):    
+#2. feladat
+db_szam = 0
+db_betu = 0
+
+for i in  betu:
+    if i in string.digits:
         db_szam += 1
-        
-print(db_szam)        
-# ================================================================
+
+for i in  betu:
+    if i in string.ascii_letters:
+        db_betu += 1
+
+print (db_szam)
+
+print (db_betu)
 
